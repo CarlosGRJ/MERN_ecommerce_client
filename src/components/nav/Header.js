6e06dace8292e072f953f754ee39a11fc8eae56c
CarlosGRJ <carlosgrj@outlook.com>
2021-04-12 14:50:26 -0500

    React Router Dom header"

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,32 +1,42 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
 import {
-   MailOutlined,
    AppstoreOutlined,
    SettingOutlined,
+   UserAddOutlined,
+   UserOutlined,
 } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 export const Header = () => {
-   const [current, setCurrent] = useState('');
+   const [current, setCurrent] = useState('home');
 
-   const handleClick = () => {
-      //
+   const handleClick = (e) => {
+      setCurrent(e.key);
    };
 
    return (
       <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
-         <Menu.Item key='mail' icon={<MailOutlined />}>
-            Home
-         </Menu.Item>
+         <Item key='home' icon={<AppstoreOutlined />}>
+            <Link to='/'>Home</Link>
+         </Item>
 
-         <SubMenu
-            key='SubMenu'
-            icon={<SettingOutlined />}
-            title='Register'>
-               <Menu.Item key='setting:1'>Option 1</Menu.Item>
-               <Menu.Item key='setting:2'>Option 2</Menu.Item>
+         <Item
+            key='register'
+            icon={<UserAddOutlined />}
+            className='float-right'>
+            <Link to='/register'>Register</Link>
+         </Item>
+
+         <Item key='login' icon={<UserOutlined />} className='float-right'>
+            <Link to='/login'>Login</Link>
+         </Item>
+
+         <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Username'>
+            <Item key='setting:1'>Option 1</Item>
+            <Item key='setting:2'>Option 2</Item>
          </SubMenu>
       </Menu>
    );
