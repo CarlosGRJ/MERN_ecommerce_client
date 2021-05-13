339cce327a796d5334c1449d8d6d3a6f56a4c45f
CarlosGRJ <carlosgrj@outlook.com>
2021-05-13 16:45:03 -0500

    Password Update

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import { ForgotPassword } from './pages/auth/ForgotPassword';
 import { currentUser } from './functions/auth';
 import { History } from './pages/user/History';
 import { UserRoute } from './routes/UserRoute';
+import { Password } from './pages/user/Password';
+import { Wishlist } from './pages/user/Wishlist';
 
 const App = () => {
    const dispatch = useDispatch();
@@ -61,6 +63,8 @@ const App = () => {
             />
             <Route exact path='/forgot/password' component={ForgotPassword} />
             <UserRoute exact path='/user/history' component={History} />
+            <UserRoute exact path='/user/password' component={Password} />
+            <UserRoute exact path='/user/wishlist' component={Wishlist} />
          </Switch>
       </>
    );
---
+++ src/pages/user/Wishlist.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { UserNav } from '../../components/nav/UserNav'
+
+export const Wishlist = () => {
+    return (
+        <div className='container-fluid'>
+         <div className='row'>
+            <div className='col-md-2'>
+               <UserNav />
+            </div>
+            <div className='col'>user wishlist page</div>
+         </div>
+      </div>
+    )
+}
