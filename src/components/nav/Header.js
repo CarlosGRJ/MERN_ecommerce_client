6f7aa75deeafb740ff2858e35de22236d9a9c0e6
CarlosGRJ <carlosgrj@outlook.com>
2021-05-17 09:46:00 -0500

    Dinamic Dashboard Link

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -57,9 +57,23 @@ export const Header = () => {
          )}
 
          {user && (
-            <SubMenu key='SubMenu' icon={<SettingOutlined />} title={user.email  && user.email.split('@')[0]} className='float-right'>
-               <Item key='setting:1'>Option 1</Item>
-               <Item key='setting:2'>Option 2</Item>
+            <SubMenu
+               key='SubMenu'
+               icon={<SettingOutlined />}
+               title={user.email && user.email.split('@')[0]}
+               className='float-right'>
+               {user && user.role === 'subscriber' && (
+                  <Item>
+                     <Link to='/user/history'>Dashboard</Link>
+                  </Item>
+               )}
+
+               {user && user.role === 'admin' && (
+                  <Item>
+                     <Link to='/admin/dashboard'>Dashboard</Link>
+                  </Item>
+               )}
+
                <Item icon={<LogoutOutlined />} onClick={logout}>
                   Logout
                </Item>
