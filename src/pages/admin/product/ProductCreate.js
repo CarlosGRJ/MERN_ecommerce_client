65fdafc0b07fed8238aa4b1c1bf6760a9eb3c187
CarlosGRJ <carlosgrj@outlook.com>
2021-05-25 09:40:38 -0500

    Code Refactor - ProductCreateForm

--- src/pages/admin/product/ProductCreate.js
+++ src/pages/admin/product/ProductCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
+import { ProductCreateForm } from '../../../components/forms/ProductCreateForm';
 import { AdminNav } from '../../../components/nav/AdminNav';
 import { createProduct } from '../../../functions/product';
 
@@ -72,90 +73,11 @@ export const ProductCreate = () => {
                <h4>Product Create</h4>
                <hr />
 
-               <form onSubmit={handleSubmit}>
-                  <div className='form-group'>
-                     <label>Title</label>
-                     <input
-                        type='text'
-                        name='title'
-                        className='form-control'
-                        value={title}
-                        onChange={handleChange}
-                     />
-                  </div>
-                  <div className='form-group'>
-                     <label>Description</label>
-                     <input
-                        type='text'
-                        name='description'
-                        className='form-control'
-                        value={description}
-                        onChange={handleChange}
-                     />
-                  </div>
-                  <div className='form-group'>
-                     <label>Price</label>
-                     <input
-                        type='number'
-                        name='price'
-                        className='form-control'
-                        value={price}
-                        onChange={handleChange}
-                     />
-                  </div>
-                  <div className='form-group'>
-                     <label>Shipping</label>
-                     <select
-                        name='shipping'
-                        className='form-control'
-                        onChange={handleChange}>
-                        <option>Please select</option>
-                        <option value='No'>No</option>
-                        <option value='Yes'>Yes</option>
-                     </select>
-                  </div>
-                  <div className='form-group'>
-                     <label>Quantity</label>
-                     <input
-                        type='number'
-                        name='quantity'
-                        className='form-control'
-                        value={quantity}
-                        onChange={handleChange}
-                     />
-                  </div>
-                  <div className='form-group'>
-                     <label>Color</label>
-                     <select
-                        name='color'
-                        className='form-control'
-                        onChange={handleChange}>
-                        <option>Please select</option>
-                        {colors.map((c) => (
-                           <option key={c} value={c}>
-                              {c}
-                           </option>
-                        ))}
-                     </select>
-                  </div>
-
-                  <div className='form-group'>
-                     <label>Brand</label>
-                     <select
-                        name='brand'
-                        className='form-control'
-                        onChange={handleChange}>
-                        <option>Please select</option>
-                        {brands.map((b) => (
-                           <option key={b} value={b}>
-                              {b}
-                           </option>
-                        ))}
-                     </select>
-                  </div>
-
-                  <button className='btn btn-outline-info'>Save</button>
-               </form>
+               <ProductCreateForm
+                  handleSubmit={handleSubmit}
+                  handleChange={handleChange}
+                  values={values}
+               />
             </div>
          </div>
       </div>
