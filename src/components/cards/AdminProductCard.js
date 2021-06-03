3842a6a3a04d05ebb8977fea067456d17a192f22
CarlosGRJ <carlosgrj@outlook.com>
2021-06-03 09:53:31 -0500

    Delete Product

--- src/components/cards/AdminProductCard.js
+++ src/components/cards/AdminProductCard.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { Card } from 'antd';
+import laptop from '../../images/macbookpro.webp'; // DEFAULT IMAGE
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
-export const AdminProductCard = ({ product }) => {
+export const AdminProductCard = ({ product, handleRemove }) => {
    // destructure
-   const { title, description, images } = product;
+   const { title, description, images, slug } = product;
 
    return (
       <Card
          cover={
             <img
-               src={images && images.length ? images[0].url : ''}
+               src={images && images.length ? images[0].url : laptop}
                style={{ height: '150px', objectFit: 'cover' }}
                className='p-1'
                alt=''
             />
-         }>
-         <Meta title={title} description={description} />
+         }
+         actions={[
+            <EditOutlined className='text-primary' />,
+            <DeleteOutlined
+               onClick={() => handleRemove(slug)}
+               className='text-danger'
+            />,
+         ]}>
+         <Meta
+            title={title}
+            description={`${description && description.substring(0, 40)}...`}
+         />
       </Card>
    );
 };
--- src/pages/admin/product/AllProducts.js
+++ src/pages/admin/product/AllProducts.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 import { AdminProductCard } from '../../../components/cards/AdminProductCard';
 import { AdminNav } from '../../../components/nav/AdminNav';
-import { getProductsByCount } from '../../../functions/product';
+import { getProductsByCount, removeProduct } from '../../../functions/product';
 
 export const AllProducts = () => {
    const [products, setProducts] = useState([]);
    const [loading, setLoading] = useState(false);
+   // redux
+   const { user } = useSelector((state) => ({ ...state }));
 
    useEffect(() => {
       loadAllProducts();
@@ -25,6 +29,27 @@ export const AllProducts = () => {
          });
    };
 
+   const handleRemove = (slug) => {
+      const answer = window.confirm(
+         'Are you sure you want to delete this product?',
+      );
+      if (answer) {
+         // console.log('send delete request', slug);
+         removeProduct(slug, user.token)
+            .then((res) => {
+               console.log('res', res);
+               loadAllProducts();
+               toast.error(`${res.data.title} is deleted`);
+            })
+            .catch((err) => {
+               console.log(err);
+               if (err.response.status === 400) {
+                  toast.error(err.response.data);
+               }
+            });
+      }
+   };
+
    return (
       <div className='container-fluid'>
          <div className='row'>
@@ -40,8 +65,11 @@ export const AllProducts = () => {
                )}
                <div className='row'>
                   {products.map((product) => (
-                     <div className='col-md-4' key={product._id}>
-                        <AdminProductCard product={product} />
+                     <div className='col-md-4 pb-3' key={product._id}>
+                        <AdminProductCard
+                           product={product}
+                           handleRemove={handleRemove}
+                        />
                      </div>
                   ))}
                </div>
