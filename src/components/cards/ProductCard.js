c116c760ff07a17f7c711d995606324b87706002
CarlosGRJ <carlosgrj@outlook.com>
2021-07-02 16:11:22 -0500

    Filter By Price Range

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -9,7 +9,7 @@ const { Meta } = Card;
 
 export const ProductCard = ({ product }) => {
    // destructure
-   const { title, description, images, slug } = product;
+   const { title, description, images, slug, price } = product;
 
    return (
       <>
@@ -37,7 +37,7 @@ export const ProductCard = ({ product }) => {
                </>,
             ]}>
             <Meta
-               title={title}
+               title={`${title} - $${price}`}
                description={`${description && description.substring(0, 40)}...`}
             />
          </Card>
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Menu, Slider } from 'antd';
+import { DollarOutlined } from '@ant-design/icons';
 import { ProductCard } from '../components/cards/ProductCard';
 import {
    getProductsByCount,
    fetchProductsByFilter,
 } from '../functions/product';
+import { types } from '../types/types';
+
+const { SubMenu, ItemGroup } = Menu;
 
 export const Shop = () => {
    const [products, setProducts] = useState([]);
    const [loading, setLoading] = useState(false);
+   const [price, setPrice] = useState([0, 0]);
+   const [ok, setOk] = useState(false);
 
+   const dispatch = useDispatch();
    const { search } = useSelector((state) => ({ ...state }));
    const { text } = search;
 
    useEffect(() => {
       loadAllProducts();
    }, []);
+
+   const fetchProducts = (arg) => {
+      fetchProductsByFilter(arg).then((res) => {
+         setProducts(res.data);
+      });
+   };
 
    // 1. load products by default on page load
    const loadAllProducts = () => {
@@ -33,18 +47,54 @@ export const Shop = () => {
       return () => clearTimeout(delayed);
    }, [text]);
 
-   const fetchProducts = (arg) => {
-      fetchProductsByFilter(arg).then((res) => {
-         setProducts(res.data);
+   // 3. load products based on price range
+   useEffect(() => {
+      // console.log('ok to request');
+      fetchProducts({ price });
+   }, [ok]);
+
+   const handleSlider = (value) => {
+      dispatch({
+         type: types.searchQuery,
+         payload: { text: '' },
       });
+      setPrice(value);
+      setTimeout(() => {
+         setOk(!ok);
+      }, 300);
    };
 
    return (
       <div className='container-fluid'>
          <div className='row'>
-            <div className='col-md-3'>search/filter menu</div>
+            <div className='col-md-3 pt-2'>
+               <h4>Search/Filter</h4>
+               <hr />
 
-            <div className='col-md-9'>
+               <Menu defaultOpenKeys={['1', '2']} mode='inline'>
+                  <SubMenu
+                     key='1'
+                     title={
+                        <span className='h6'>
+                           <DollarOutlined />
+                           Price
+                        </span>
+                     }>
+                     <div>
+                        <Slider
+                           className='mx-4'
+                           tipFormatter={(v) => `$${v}`}
+                           range
+                           value={price}
+                           onChange={handleSlider}
+                           max='4999'
+                        />
+                     </div>
+                  </SubMenu>
+               </Menu>
+            </div>
+
+            <div className='col-md-9 pt-2'>
                {loading ? (
                   <h4 className='text-danger'>Loading...</h4>
                ) : (
