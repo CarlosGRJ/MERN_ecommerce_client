2aff20825d3dd4afa9eef70b4365ecf5d7492f4e
CarlosGRJ <carlosgrj@outlook.com>
2021-07-09 18:52:04 -0500

    Cart Items in Table

---
+++ src/components/cards/ProductCardInCheckout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const ProductCardInCheckout = ({p}) => {
+    return (
+        <tbody>
+            <tr>
+                <td>Image</td>
+                <td>{p.title}</td>
+                <td>{p.price}</td>
+                <td>{p.brand}</td>
+                <td>{p.color}</td>
+                <td>{p.count}</td>
+                <td>Shipping Icon</td>
+                <td>Delete Icon</td>
+            </tr>
+        </tbody>
+    )
+}
