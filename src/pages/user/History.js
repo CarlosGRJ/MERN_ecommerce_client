d6c9afdce28404431d2e820b56289146deea3240
CarlosGRJ <carlosgrj@outlook.com>
2021-07-22 16:25:17 -0500

    Showing Each Order's Products in Table

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -20,13 +20,68 @@ export const History = () => {
       });
    };
 
+   const showOrderInTable = (order) => (
+      <table className='table table-bordered'>
+         <thead className='thead-light'>
+            <tr>
+               <th scope='col'>Title</th>
+               <th scope='col'>Price</th>
+               <th scope='col'>Brand</th>
+               <th scope='col'>Color</th>
+               <th scope='col'>Count</th>
+               <th scope='col'>Shipping</th>
+            </tr>
+         </thead>
+         <tbody>
+            {order.products.map((p) => (
+               <tr key={p._id}>
+                  <td>
+                     <b>{p.product.title}</b>
+                  </td>
+                  <td>${p.product.price}</td>
+                  <td>{p.product.brand}</td>
+                  <td>{p.color}</td>
+                  <td>{p.count}</td>
+                  <td>
+                     {p.product.shipping === 'Yes' ? (
+                        <CheckCircleOutlined className='text-success' />
+                     ) : (
+                        <CloseCircleOutlined className='text-danger' />
+                     )}
+                  </td>
+               </tr>
+            ))}
+         </tbody>
+      </table>
+   );
+
+   const showEachOrders = () =>
+      orders.map((order) => (
+         <div className='m-5 p-3 card' key={order._id}>
+            <p>show payment info</p>
+            {showOrderInTable(order)}
+            <div className='row'>
+               <div className='col'>
+                  <p>PDF download</p>
+               </div>
+            </div>
+         </div>
+      ));
+
    return (
       <div className='container-fluid'>
          <div className='row'>
             <div className='col-md-2'>
                <UserNav />
             </div>
-            <div className='col'>user history page</div>
+            <div className='col text-center'>
+               <h4>
+                  {orders.length > 0
+                     ? 'User purchase orders'
+                     : 'No purchase orders'}
+               </h4>
+               {showEachOrders()}
+            </div>
          </div>
       </div>
    );
