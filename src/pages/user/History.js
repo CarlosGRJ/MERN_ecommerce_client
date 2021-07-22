aee10e3395478c568b0b848d48dc1bf9fc7b0660
CarlosGRJ <carlosgrj@outlook.com>
2021-07-22 15:14:42 -0500

    User Orders in Purchase History Page

--- src/pages/user/History.js
+++ src/pages/user/History.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserNav } from '../../components/nav/UserNav';
+import { getUserOrders } from '../../functions/user';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
 
 export const History = () => {
+   const [orders, setOrders] = useState([]);
+   const { user } = useSelector((state) => ({ ...state }));
+
+   useEffect(() => {
+      loadUserOrders(user);
+   }, [user]);
+
+   const loadUserOrders = (user) => {
+      getUserOrders(user.token).then((res) => {
+         console.log('res ', res);
+         setOrders(res.data);
+      });
+   };
+
    return (
       <div className='container-fluid'>
          <div className='row'>
-            <div className="col-md-2">
+            <div className='col-md-2'>
                <UserNav />
             </div>
             <div className='col'>user history page</div>
